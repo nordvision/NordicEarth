4234e6db5c396d5b9a14808d5c5cb84f0b3bff72
Leevi Heino <leeviheino@live.fi>
2019-11-14 17:00:44 +0000

    GIF animation

--- front/src/pages/HomePage.js
+++ front/src/pages/HomePage.js
@@ -4,9 +4,7 @@ import './HomePage.scss'
 import Header from '../Header'
 import team1Image from '../assets/team1.png'
 import team2Image from '../assets/team2.png'
-import hmWireframe from '../assets/hm_wireframe.png'
-import hmOverflate from '../assets/hm_overflate.png'
-import hmTekstur from '../assets/hm_tekstur.png'
+import hmGif from '../assets/hm-wireframe.gif'
 
 /* eslint-disable max-len */
 
@@ -23,13 +21,11 @@ function HomePage() {
             <li>As a journalist, you can use this expert tool to generate 3D image from a point of interest.</li>
             <li>You can also make cool CGI panoramic content for your videos or articles.</li>
           </ul>
+          <h3>Combining topographical data</h3>
+          <img alt="height map gif" src={hmGif} />
           <h2>Meet the geniuses behind this project</h2>
           <img alt="geniuses 1" src={team1Image} />
           <img alt="geniuses 2" src={team2Image} />
-          <h2>Extreme topography</h2>
-          <img alt="height map wireframe" src={hmWireframe} />
-          <img alt="height map overflate" src={hmOverflate} />
-          <img alt="height map texture" src={hmTekstur} />
         </div>
       </div>
     </div>
