c97d7e01f0f2260d5be3bd5f5a2b8d6693354d7e
Kristoffer Dyrkorn <kristoffer.dyrkorn@nrk.no>
2019-11-14 16:06:14 +0100

    Improve the example code and add more comments.

--- front/src/ThreeModule.js
+++ front/src/ThreeModule.js
@@ -5,11 +5,11 @@ export default class ThreeModule {
     // In three.js, everything to be drawn must be added to the Scene object
     this.scene = new THREE.Scene();
 
-    // Define a light, shining with white color
-    // and being located at x, y, z coordinate (100, 100, 200)
-    this.light = new THREE.DirectionalLight(0xffffff);
-    this.light.position.set(100, 100, 200);
+    // Define a point light (i.e. a light bulb), shining with white color
+    this.light = new THREE.PointLight(0xffffff);
+    this.light.position.set(25, 50, 20);
 
+    // Add the light to the scene
     this.scene.add(this.light);
 
     // Define the camera - having a 45 degree field of view
@@ -23,7 +23,7 @@ export default class ThreeModule {
     );
 
     // Define the camera position (x, y, z)
-    this.camera.position.set(10, 10, 20);
+    this.camera.position.set(0, -20, 10);
 
     // Fix up the camera coordinate conventions - so that the
     // x and y coordinates run along the surface of our terrain
@@ -33,28 +33,43 @@ export default class ThreeModule {
 
     // Set up WebGL and attach a canvas element to the DOM
     this.renderer = new THREE.WebGLRenderer();
+
+    // Set the canvas element to be full screen
     this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Adjust pixel size for retina screens
     this.renderer.setPixelRatio(window.devicePixelRatio);
+
+    // Add the canvas element Three.js renders to onto the DOM
     document.body.appendChild(this.renderer.domElement);
 
+    // Bind the resize handler function before using it
     this.resizeHandler = this.resizeHandler.bind(this);
 
-    //        window.addEventListener("keydown", keyboardHandler);
+    // Attach the function to the event
     window.addEventListener("resize", this.resizeHandler, false);
 
-    // For now, lets render three lines describing the x, y and z axes
-    const helper = new THREE.AxesHelper(1000);
-    this.scene.add(helper);
+    //        window.addEventListener("keydown", keyboardHandler);
 
-    // For now, add a ground plane 1000 long and 1000 units wide
-    const geometry = new THREE.PlaneBufferGeometry(1000, 1000);
-    const material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+    // Uncomment this code to render three lines on the screen, each describing the x, y and z axes
+    // const helper = new THREE.AxesHelper(1000);
+    // this.scene.add(helper);
+
+    // For now, add a ground plane 200 units long and 200 units wide
+    // consisting of 256 grid segments in each direction
+    const geometry = new THREE.PlaneBufferGeometry(200, 200, 256, 256);
+
+    // Define a grey colored material, having smooth shading
+    const material = new THREE.MeshPhongMaterial({ color: 0xcccccc });
+
+    // Define this to be a Mesh
     const ground = new THREE.Mesh(geometry, material);
 
+    // Add the ground to the scene
     this.scene.add(ground);
 
+    // Bind the animate function before using it
     this.animate = this.animate.bind(this);
-
     this.animate();
   }
 
@@ -64,9 +79,11 @@ export default class ThreeModule {
   }
 
   resizeHandler() {
-    debugger;
+    // Adjust the camera aspect according to the window aspect
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
+
+    // Also adjust the canvas element size so it stays full screen
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 }
