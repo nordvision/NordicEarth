368301a5a1416f091a7de4aea2dd48a8628dfc28
Peter Freltoft     Web og Apps <pefr@dr.dk>
2019-11-14 15:42:03 +0000

    Text styling

--- front/src/pages/HomePage.js
+++ front/src/pages/HomePage.js
@@ -10,8 +10,10 @@ function HomePage() {
     <div>
       <Header />
       <div className="HomePage">
-        <h1>Experience the magic of Nordic Earth</h1>
-        <p>Lorem ipsum dolor amet pork belly schlitz aute, aliquip craft beer veniam tilde selfies dolore ennui synth vape jianbing. Bespoke chia cloud bread poke, snackwave pariatur pop-up iceland. Wayfarers adipisicing elit church-key cornhole wolf health goth. Selvage poutine banh mi, ut iceland duis palo santo microdosing letterpress tumeric affogato before they sold out ut ex. Austin mollit bicycle rights, cronut vice cred cardigan exercitation actually do raw denim. Everyday carry pour-over officia, art party mlkshk next level cray. Reprehenderit actually hot chicken kitsch mustache.</p>
+        <div className="HomePage_inner">
+          <h1>Experience the magic of Nordic Earth</h1>
+          <p>Lorem ipsum dolor amet pork belly schlitz aute, aliquip craft beer veniam tilde selfies dolore ennui synth vape jianbing. Bespoke chia cloud bread poke, snackwave pariatur pop-up iceland. Wayfarers adipisicing elit church-key cornhole wolf health goth. Selvage poutine banh mi, ut iceland duis palo santo microdosing letterpress tumeric affogato before they sold out ut ex. Austin mollit bicycle rights, cronut vice cred cardigan exercitation actually do raw denim. Everyday carry pour-over officia, art party mlkshk next level cray. Reprehenderit actually hot chicken kitsch mustache.</p>
+        </div>
       </div>
     </div>
   )
